6c4a0c95b9500041b9342e14ea0152a665adc506
Paulo Sergio Romao Junior <PauloSergioRomaoJunior@users.noreply.github.com>
2017-03-12 18:24:52 -0300

    Create db.php

---
+++ lib/Sockets/config/db.php
@@ -0,0 +1,12 @@
+<?php  
+	date_default_timezone_set("America/Sao_Paulo");
+
+
+	define('HOST', 'localhost');
+	define('PORT', 3306);
+	define('DBNAME','dbname');
+	define('USER', 'user');
+	define('PASSWORD','senha');
+
+
+?>
